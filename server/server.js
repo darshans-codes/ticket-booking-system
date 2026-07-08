b70535333149717cf296c4c8a60e889fa6546e76
darshans-codes <darshan.nighthawk2025@gmail.com>
2026-07-08 21:09:56 +0530

    Setup express server and mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Server is running...");
+  res.send("API is running...");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
