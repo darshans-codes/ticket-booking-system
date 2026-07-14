774c0780ea06040c48e42068cd262b9becaf0a37
darshans-codes <darshan.nighthawk2025@gmail.com>
2026-07-14 18:35:11 +0530

    Implemented user auth API

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,4 +28,51 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
