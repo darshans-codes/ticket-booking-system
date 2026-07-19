fe288986a80323c7dbb1a75fcd52c18dd64311a7
darshans-codes <darshan.nighthawk2025@gmail.com>
2026-07-19 11:44:36 +0530

    Final project

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/ticket_booking_db");
+    const conn = await mongoose.connect(
+      "mongodb://127.0.0.1:27017/ticket_booking_db"
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
--- server/server.js
+++ server/server.js
@@ -20,20 +20,10 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.options("*", cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
